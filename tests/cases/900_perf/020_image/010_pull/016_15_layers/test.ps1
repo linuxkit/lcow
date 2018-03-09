a9b58a48e30d6597358c5a20b6b06a8aaaf6802d
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-03-09 16:57:30 +0000

    tests: Add performance test for pulling multi-layer images
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ tests/cases/900_perf/020_image/010_pull/016_15_layers/test.ps1
@@ -0,0 +1,13 @@
+# SUMMARY: Pull an image with fifteen layers
+# LABELS:
+# REPEAT:
+
+$libBase = Join-Path -Path $env:RT_PROJECT_ROOT -ChildPath _lib
+$lib = Join-Path -Path $libBase -ChildPath lib.ps1
+. $lib
+
+$imageName = "linuxkit/test-lcow:15layers"
+
+docker image pull --platform linux $imageName
+if ($lastexitcode -ne 0) { exit 1 }
+exit 0
