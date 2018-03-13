37c5aabb51e172685aec2127abe793a846415a64
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-03-13 12:58:20 +0000

    tests: Add build performance tests
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ tests/cases/900_perf/020_image/020_build/012_5_layers/test.ps1
@@ -0,0 +1,13 @@
+# SUMMARY: Build an image with five layers
+# LABELS:
+# REPEAT:
+
+$libBase = Join-Path -Path $env:RT_PROJECT_ROOT -ChildPath _lib
+$lib = Join-Path -Path $libBase -ChildPath lib.ps1
+. $lib
+
+$imageName = $env:RT_TEST_NAME
+
+docker image build --no-cache --network none --platform linux -t $imageName .
+if ($lastexitcode -ne 0) { exit 1 }
+exit 0
