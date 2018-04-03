b5b17e2f4f27d93b2c1a3404422fb55ba36d001f
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-04-03 17:42:42 +0100

    tests: Add a link to a flock() related issue
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- tests/cases/100_hostvol/050_flock_volume/test.ps1
+++ tests/cases/100_hostvol/050_flock_volume/test.ps1
@@ -1,6 +1,7 @@
 # SUMMARY: Check that flock() works on a shared volume
 # LABELS:
 # REPEAT:
+# ISSUE: https://github.com/moby/moby/issues/36531#issuecomment-372020535
 
 $libBase = Join-Path -Path $env:RT_PROJECT_ROOT -ChildPath _lib
 $lib = Join-Path -Path $libBase -ChildPath lib.ps1
