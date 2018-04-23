077fa7f737c0935dc570b2d91df0b802d8e133be
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-04-23 16:43:46 +0100

    tests: Convert many files benchmarks to use rtf benchmarks
    This allows us to move the setup from the group initialiser
    into the test itself.
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- tests/cases/900_perf/100_hostvol/020_single_dir/010_stat_5000_files/run.sh
+++ tests/cases/900_perf/100_hostvol/020_single_dir/010_stat_5000_files/run.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 set -e
-# don't set -x as it creates a lot of noise
 
 DIR=$1
 
--- tests/cases/900_perf/100_hostvol/020_single_dir/050_stat_touch_stat_5000_files/test.ps1
+++ tests/cases/900_perf/100_hostvol/020_single_dir/050_stat_touch_stat_5000_files/test.ps1
@@ -8,15 +8,30 @@ $lib = Join-Path -Path $libBase -ChildPath lib.ps1
 
 $ret = 0
 
-$testPath = Join-Path -Path $env:TEST_TMP_ROOT -ChildPath "single-dir-touch-stat"
+$testPath = $env:TEST_TMP
+Remove-Item -Force -Recurse -ErrorAction Ignore -Path $testPath
+New-Item -ItemType Directory -Force -Path $testPath
+$i = 0
+While ($i -lt 5000) {
+    $fileName = "file-stat-" + $i
+    $filePath = Join-Path -Path $testPath -ChildPath $fileName
+    New-Item -ItemType File -Path $filePath
+    $i += 1
+}
+
+$start = Get-Date
 
 $p = [string]$pwd.Path
 docker container run --platform linux --rm `
   -v $testPath`:/test `
   -v $p`:/script `
   alpine:3.7 sh /script/run.sh /test
-if ($lastexitcode -ne 0) {
-    $ret = 1
-}
+if ($lastexitcode -ne 0) { $ret = 1 }
+
+$end = Get-Date
+$diff = (($end - $start).TotalSeconds).ToString("#.##s")
+Write-Output "RT_BENCHMARK_RESULT: $diff"
+
+Remove-Item -Force -Recurse -ErrorAction Ignore -Path $testPath
 
 exit $ret
